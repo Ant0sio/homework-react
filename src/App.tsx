0e48bd90374c466aa3617959c806d75a6d0ab86a
Ant0sio <antonzeninec@gmail.com>
2025-03-03 21:36:54 +0200

    React Homework 1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import FamilyComponent from "./MyComponents/FamilyComponent/FamilyComponent.tsx";
+function App() {
+
+
+  return (
+    <>
+          <FamilyComponent/>
+    </>
+  )
+}
+
+export default App
+
